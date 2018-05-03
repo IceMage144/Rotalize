33c8a749b702cdf7dbd815ef3b02581894968039
PaiZuZe <vitinho_cga@hotmail.com>
2018-05-03 20:39:22 -0300

    Adds models for trip and day. -B

---
+++ app/models/day.rb
@@ -0,0 +1,14 @@
+class Day < ApplicationRecord
+    has_may :hotels	
+    has_many :museums
+    has_many :monuments
+    has_many :restaurants
+    
+    def initialize (name, day. month, year)
+		@name = name
+		@day = day
+		@month = month
+		@year = year
+	end
+    
+end
