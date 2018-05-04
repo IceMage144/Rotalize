e3afbf8cfea27bb65124cef7f7d24bbd813d0e5a
PaiZuZe <vitinho_cga@hotmail.com>
2018-05-04 16:36:57 -0300

    Adds migrations to the DB. -B

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417181107) do
+ActiveRecord::Schema.define(version: 20180504193407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cities", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "days", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -36,6 +41,11 @@ ActiveRecord::Schema.define(version: 20180417181107) do
   end
 
   create_table "restaurants", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trips", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
