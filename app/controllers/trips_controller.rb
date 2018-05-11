b29aef875ebea066dbdfcde0c119059bd33a0a60
Zeke <vitinho_cga@hotmail.com>
2018-05-11 11:52:12 -0300

    Changes the model and the controller for trip. -Z

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,20 +4,42 @@ class TripsController < ApplicationController
     end
 
 	def create
-	end
+        @trip = Trip.new(trip_params) #cria a trip
+        if(@trip.save)  #salva a trip se tem os params certos de acordo com o modelo
+            redirect_to @trip #vai para o show
+        else
+            render 'new' #volta para o new se deu ruim, colocar um erro na view
+        end
+    end
 
 	def new
+        @trip = Trip.new
 	end
 
 	def edit
+        @trip = Trip.find(params[:id])
 	end
 
 	def show
+        @trip = Trip.find(params[:id])
 	end
 
 	def update
+        @trip = Trip.find(params[:id])
+        if(@trip.update(trip_params))
+            redirect_to @trip
+        else
+            render 'edit'
+        end
 	end
 
 	def destroy
+        @trip = Trip.find(params[:id])
+        @post.destroy
+        redirect_to post_path
+    end
+
+    private def trip_params
+        params.require(:trip).permit(:name, :beginning, :durarion) #precisa desses paraetros
     end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,11 +1,8 @@
 class Trip < ApplicationRecord
 	has_many :days
 	belongs_to :user
-	
-	def initialize (name, beginning, durarion)
-		@name = name
-		@beginning = beginning
-		@durarion = durarion
-	end
+	validates :name, presence: true,
+	validates :beginning, presence: true,
+	validates :durarion, presence: true,
 
 end
