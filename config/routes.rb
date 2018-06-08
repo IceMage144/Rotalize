4a35c9ebde6106da06229f2bd7e74923e5784983
IceMage144 <jgabriel.basi@hotmail.com>
2018-06-08 11:18:10 -0300

    Add registration controller for accepting new user name atribute

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get 'errors/not_found'
 
   get 'errors/internal_server_error'
@@ -9,7 +10,7 @@ Rails.application.routes.draw do
   match "/404", :to => "errors#not_found", :via => :all
   match "/500", :to => "errors#internal_server_error", :via => :all
 
-  devise_for :users
+  devise_for :users, :controllers => {registrations: 'registrations'}
   resources :trips
   resources :places
 end
