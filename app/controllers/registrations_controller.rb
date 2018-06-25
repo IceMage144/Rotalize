67b693362f5dc6a945374644c0ab29b345f58dc5
Raphael R. Gusmao <raphael.rgusmao@gmail.com>
2018-06-25 17:28:37 -0300

    c h a n g e s

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,25 @@
 class RegistrationsController < Devise::RegistrationsController
+
+  def update
+    respond_to do |format|
+      if current_user.update( account_update_params)
+        format.html { redirect_to current_user, notice: 'Current_user was successfully updated.' }
+        format.json { render :show, status: :ok, location: current_user }
+      else
+        format.html { render :edit }
+        format.json { render json: current_user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :img, :password, :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:name, :email, :img, :password, :password_confirmation, :current_password)
   end
 
 end
