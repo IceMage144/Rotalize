dac6ef8744197aea92bbc8a01a865744b8a6f44d
IceMage144 <jgabriel.basi@hotmail.com>
2018-07-02 18:05:40 -0300

    Remove error pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,6 @@ Rails.application.routes.draw do
 
   get 'errors/internal_server_error'
 
-  match "/404", :to => "errors#not_found", :via => :all
-  match "/500", :to => "errors#internal_server_error", :via => :all
-
   devise_for :users, :controllers => {registrations: 'registrations'}
 
   resources :trips
